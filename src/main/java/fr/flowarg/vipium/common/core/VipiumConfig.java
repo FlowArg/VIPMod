f657cffbcc85e21e43b9490e5d27d3431ff972bb
Flow Arg <coutureflorence3@gmail.com>
2021-01-29 19:19:39 +0100

    removes useless config

--- src/main/java/fr/flowarg/vipium/common/core/VipiumConfig.java
+++ src/main/java/fr/flowarg/vipium/common/core/VipiumConfig.java
@@ -24,12 +24,6 @@ public class VipiumConfig
         private final BooleanValue showRealms;
         private final BooleanValue showUselessOptions;
         private final BooleanValue enableRPC;
-        private final BooleanValue enableHelmetEffect;
-        private final BooleanValue enableChestplateEffect;
-        private final BooleanValue enableLeggingsEffect;
-        private final BooleanValue enableBootsEffect;
-        private final BooleanValue enableFirstFullEffect;
-        private final BooleanValue enableSecondFullEffect;
 
         public Client(@Nonnull Builder builder)
         {
@@ -42,18 +36,6 @@ public class VipiumConfig
                     .define("showUselessOptions", false);
             this.enableRPC = builder.comment(" Enable/Disable the Discord Rich Presence for VIP.")
                     .define("enableRPC", true);
-            this.enableHelmetEffect = builder.comment(" Enable/Disable the effect given by the Pure Vipium Helmet")
-                    .define("enableHelmetEffect", true);
-            this.enableChestplateEffect = builder.comment(" Enable/Disable the effect given by the Pure Vipium Chestplate")
-                    .define("enableChestplateEffect", true);
-            this.enableLeggingsEffect = builder.comment(" Enable/Disable the effect given by Pure Vipium Leggings")
-                    .define("enableLeggingsEffect", true);
-            this.enableBootsEffect = builder.comment(" Enable/Disable the effect given by Pure Vipium Boots")
-                    .define("enableBootsEffect", true);
-            this.enableFirstFullEffect = builder.comment(" Enable/Disable the first effect given by the complete Pure Vipium Armor")
-                    .define("enableFirstFullEffect", true);
-            this.enableSecondFullEffect = builder.comment(" Enable/Disable the second effect given by the complete Pure Vipium Armor")
-                    .define("enableSecondFullEffect", true);
 
             builder.pop();
         }
@@ -71,36 +53,6 @@ public class VipiumConfig
         public BooleanValue getEnableRPC()
         {
             return this.enableRPC;
-        }
-
-        public BooleanValue getEnableHelmetEffect()
-        {
-            return this.enableHelmetEffect;
-        }
-
-        public BooleanValue getEnableChestplateEffect()
-        {
-            return this.enableChestplateEffect;
-        }
-
-        public BooleanValue getEnableLeggingsEffect()
-        {
-            return this.enableLeggingsEffect;
-        }
-
-        public BooleanValue getEnableBootsEffect()
-        {
-            return this.enableBootsEffect;
-        }
-
-        public BooleanValue getEnableFirstFullEffect()
-        {
-            return this.enableFirstFullEffect;
-        }
-
-        public BooleanValue getEnableSecondFullEffect()
-        {
-            return this.enableSecondFullEffect;
         }
     }
 }

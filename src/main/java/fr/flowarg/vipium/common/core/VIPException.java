f176e5ed5caebbc1ecd11b83f17218fe8fa6b102
FlowArg <coutureflorence3@gmail.com>
2021-08-04 11:08:58 +0200

    Cleanedup project

--- src/main/java/fr/flowarg/vipium/common/core/VIPException.java
+++
@@ -1,9 +0,0 @@
-package fr.flowarg.vipium.common.core;
-
-public class VIPException extends Exception
-{
-    public VIPException(String message, Throwable cause)
-    {
-        super(message, cause);
-    }
-}
